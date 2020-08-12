dff46ef79e12715b481e7abb2279fd2188fd7cab
MMendes24 <mmbusiness1@protonmail.com>
2020-08-12 14:51:51 -0400

    Main component created and connected with react-redux. Rendering a test header for the time being.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import LiturgicalCalendar from './components/LiturgicalCalendar'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <LiturgicalCalendar />
     </div>
   );
 }
---
+++ app/src/components/LiturgicalCalendar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const LiturgicalCalendar = (props) => {
+    return (
+        <h1>Liturgical Calendar</h1>
+    )
+}
+
+export default connect(null, {})(LiturgicalCalendar)
